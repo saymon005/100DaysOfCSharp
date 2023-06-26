c017f47a97fb2c7540088f45a03411ad0ab18058
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-26 22:51:48 +0600

    Add project files.

---
+++ Day2_DataType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_DataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
