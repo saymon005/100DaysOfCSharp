09abd966a5fa5a8f168f4cd993d02bf7195ac240
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-27 23:22:48 +0600

    Study on various loop

--- Day6_Loop/Program.cs
+++ Day6_Loop/Program.cs
@@ -12,25 +12,36 @@ namespace Day6_Loop
         {
             
             int i = 1; //initialization
-            while (i <= 10)   //condition
-            {
-                Console.WriteLine(i);
-                i++;  // update
-            }
+            //while (i <= 10)   //condition
+            //{
+            //    Console.WriteLine(i);
+            //    i++;  // update
+            //}
 
             
 
-            for (i = 1; i <= 10; i++)
-            {
-                Console.WriteLine(i);
-            }
+            //for (i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
-            i = 1;
-            do
+            //i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}while (i <= 10);
+
+            // Nested loop
+
+            for(i = 10; i>=1; i--)
             {
-                Console.WriteLine(i);
-                i++;
-            }while (i <= 10);
+                for(int j = i; j >=1; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
