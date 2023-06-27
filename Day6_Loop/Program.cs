6c4e3c2dccf48771281109f413c7fdc098fee2ca
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-27 23:15:16 +0600

    Study on various loop

---
+++ Day6_Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Loop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 1; //initialization
+            while (i <= 10)   //condition
+            {
+                Console.WriteLine(i);
+                i++;  // update
+            }
+
+            
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
