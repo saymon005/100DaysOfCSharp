00cb49f4a43ea9b04f0fcf4c111ac16bb22dc829
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-27 11:41:10 +0600

    Basic Class Create, property, method

---
+++ Day4_BasicClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_BasicClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Saymon";
+            person.LastName = "Islam";
+            Console.WriteLine(person.GetFullName());
+            Console.WriteLine(person.FullName);
+
+            Console.ReadLine();
+        }
+    }
+}
