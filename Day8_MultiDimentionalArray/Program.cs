73e627b680b0c48f7d48b49387bbfc54c40839c0
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-29 21:35:08 +0600

    Multidimentional array and jagged array

--- Day8_MultiDimentionalArray/Program.cs
+++ Day8_MultiDimentionalArray/Program.cs
@@ -11,7 +11,28 @@ namespace Day8_MultiDimentionalArray
         static void Main(string[] args)
         {
             int[,] grades = new int[5,5]; // multidimentional array like this is 2D array
-            int[,] numbers = new int[3, 4]; // this is jagged array
+            //int[,] numbers = new int[3, 4]; // this is jagged array
+            int[,] numbers =
+            {
+                {1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12}
+
+            };
+
+            Console.WriteLine(numbers[0, 2]);
+            Console.WriteLine(numbers.GetLength(0)); // this used for how many rows have
+            Console.WriteLine(numbers.GetLength(1)); // this used for how many column have
+
+            for(int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for(int j=0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
