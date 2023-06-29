d5a124afd8d5f1ca95323276736e373c2370ea6a
saymon005 <msaymonislamiftikar@gmail.com>
2023-06-29 21:06:55 +0600

    Multidimentional array and jagged array

--- Day8_MultiDimentionalArray/Program.cs
+++ Day8_MultiDimentionalArray/Program.cs
@@ -10,7 +10,8 @@ namespace Day8_MultiDimentionalArray
     {
         static void Main(string[] args)
         {
-
+            int[,] grades = new int[5,5]; // multidimentional array like this is 2D array
+            int[,] numbers = new int[3, 4]; // this is jagged array
         }
     }
 }
